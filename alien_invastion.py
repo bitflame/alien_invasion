644a30bddeafffccbdf85cd6dd35ab0918f6ff36
bitflame <monazzam@protonmail.com>
2024-06-05 14:06:12 -0400

    I can now see how many ships I have left

--- alien_invastion.py
+++ alien_invastion.py
@@ -44,6 +44,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:   
             # Decrement ships_left
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
             # Get rid fo any remaining bullets and aliens
             self.bullets.empty()
             self.aliens.empty()
@@ -94,6 +95,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active=True
             # Get rid of any remaining bullets adn aliens
             self.bullets.empty()
